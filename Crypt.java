1d6dcaadb814263b1c445829cb7efd4073292073
naveen_21tyagi <naveentyagi68@gmail.com>
2023-04-28 00:32:09 +0530

    Encryption and Decryption utility

---
+++ Crypt.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.security.*;
+import java.security.KeyPair;
+
+import javax.crypto.Cipher;
+
+public class Crypt {
+   Cipher cipher;
+   KeyPair keyPair;
+
+   Crypt() throws Exception{
+      //Creating a Cipher object
+      cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+      FileInputStream fis=new FileInputStream("keyPair");
+      ObjectInputStream ois=new ObjectInputStream(fis);
+      keyPair=(KeyPair)ois.readObject();
+      ois.close();
+   }
+   
+   byte[] encrypt(String text)throws Exception{
+      cipher.init(Cipher.ENCRYPT_MODE, keyPair.getPublic());
+	  
+      //Add data to the cipher
+      byte[] input = text.getBytes();	  
+      cipher.update(input);
+	  
+      //encrypting the data
+      byte[] cipherText = cipher.doFinal();	 
+      // return (new String(cipherText, "UTF8"));
+      return cipherText;
+   }
+   
+   String decrypt(byte[] text)throws Exception{
+      cipher.init(Cipher.DECRYPT_MODE, keyPair.getPrivate());
+      
+      //Decrypting the text
+      byte[] decipheredText = cipher.doFinal(text);
+      return (new String(decipheredText));
+   }
+}
